5e6dc5bbee7e9260b63a86299e765cc039c68595
MAXIMO HEVIA <95097703@ort>
2025-05-30 09:03:29 -0300

    Arreglamos errores y agregamos mapa

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,27 +14,80 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+   public class HomeController : Controller
     {
-        return View();
+        private const string MapSessionKey = "_MapState"; 
+
+        public IActionResult Index()
+        {
+            return View("Index"); 
+        }
+        [HttpPost]
+        public IActionResult ResolveRoom(int roomId)
+        {
+            Sala currentSala = Objeto.StringToObject<Sala>(HttpContext.Session.GetString(MapSessionKey));
+
+            if (currentSala != null)
+            {
+                Room roomToUnlock = currentSala.Rooms.FirstOrDefault(r => r.Id == roomId);
+
+                if (roomToUnlock != null && !roomToUnlock.IsUnlocked)
+                {
+                    roomToUnlock.IsUnlocked = true;
+                    HttpContext.Session.SetString(MapSessionKey, Objeto.ObjectToString(currentSala));
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ResetMap()
+        {
+            HttpContext.Session.Remove(MapSessionKey);
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    
+    public IActionResulta IngresarNombre() 
+    {
+        return View("Presentacion/IngresarNombre");
     }
     public IActionResult Intro(string nuevoNombre)
     {
         HttpContext.Session.SetString("nombre", nuevoNombre);
-        return View();
+        return View("Presentacion/Intro");
     }
     public IActionResult Historia() 
     {
         ViewBag.nombre = HttpContext.Session.GetString("nombre");
+        return View("Presentacion/Historia");
+    }
+    public IActionResult PrimerCuarto() 
+    {
+        ViewBag.segs = 300;
+        return View("SalaI/PrimerCuarto");
+    }
+    public IActionResult Perdiste() 
+    {
         return View();
     }
     public IActionResult HistoriaII()
     {
         ViewBag.nombre = HttpContext.Session.GetString("nombre");
-        return View();
+        return View("SalaI/HistoriaII");
     }
-    public IActionResult VerificarCodigo() 
+    public IActionResult VerificarCodigo(string codigo) 
     {
-
+        if (codigo == "SATOIA") {
+            return View("SalaI/HistoriaIII");
+        }
+        else {
+            return View("SalaI/Error");
+        }
     }
 }
--- Models/Juego.cs
+++ Models/Juego.cs
@@ -1,5 +1,13 @@
 namespace TP5_Hevia_Ku.Models;
 class Juego {
-    public string nombre {get; set;}
-    public Dictionary <int, Sala> salas {get; set;}
+   public List<Sala> salas { get; set; } 
+   public Juego()
+   {
+    salas = new List<Sala>()
+            Rooms.Add(new Room { Id = 1, IsUnlocked = true });
+            Rooms.Add(new Room { Id = 2, IsUnlocked = false });
+            Rooms.Add(new Room { Id = 3, IsUnlocked = false });
+            Rooms.Add(new Room { Id = 4,  IsUnlocked = false });
+   }
+
 }
